e9d44f951775483af5cad123551eedde2870e193
Sobit Karki <sobitkarki7@gmail.com>
2024-05-21 21:31:51 +0545

    test verified commit

--- resources/views/items/create.blade.php
+++ resources/views/items/create.blade.php
@@ -7,7 +7,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="{{ asset('css/style.css') }}">
-
+    {{-- a change for test --}}
 
 </head>
 <body>
